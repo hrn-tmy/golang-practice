63c54ae707773012bb8a0895078bc830bbb68c70
hrn-tmy <mj0829mj0625@icloud.com>
2025-07-01 21:43:49 +0900

    マップとスライスの練習

--- main.go
+++ main.go
@@ -2,34 +2,19 @@ package main
 
 import "fmt"
 
-func main () {
-	var a any
-	fmt.Println(a) // 初期値：nil
+func main() {
+	m := make(map[string]string)
+	m["Bob"] = "Robert"
+	fmt.Println(m)
 
-	var b bool
-	fmt.Println(b) // 初期値：false
+	n := map[int]int{0: 1, 1: 2, 2: 3}
+	for k, v := range n {
+		fmt.Printf("key: %d, value: %d\n", k, v)
+	}
 
-	var c byte
-	fmt.Println(c) // 初期値：0
-
-	var d complex64
-	fmt.Println(d) // 初期値：(0+0i)
-
-	var e float32
-	fmt.Println(e) // 初期値：0(float64も同じ)
-
-	var f int
-	fmt.Println(f) // 初期値：0(int8,int16,int32,int64も同じ)
-
-	var g rune
-	fmt.Println(g) // 初期値：0
-
-	var h string
-	fmt.Println(h) // 初期値：(空文字)
-
-	var i uint
-	fmt.Println(i) // 初期値：0(uint8,uint16,uint32,uint64も同じ)
-	
-	var j uintptr
-	fmt.Println(j) // 初期値：0
+	s1 := []int{0, 1, 2, 3, 4, 5}
+	fmt.Println(len(s1))
+	for i, v := range s1 {
+		fmt.Printf("key: %d, value: %d\n", i, v)
+	}
 }
